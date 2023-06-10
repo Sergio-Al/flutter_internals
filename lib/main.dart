e131f9b720e1cc5b1103ac4673f80c1641f02ebe
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-06-10 15:53:33 -0400

    Understanding & Using Keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:internal_flutter/ui_updates_demo.dart';
+import 'package:internal_flutter/keys/keys.dart';
 
 void main() {
   runApp(const App());
@@ -17,7 +17,7 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Internals'),
         ),
-        body: const UIUpdatesDemo(),
+        body: Keys(),
       ),
     );
   }
